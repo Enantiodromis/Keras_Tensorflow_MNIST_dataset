d758d56eadba7dc4c3032bd222eb171a6d5b7059
Enantiodromis <georgerebradley@gmail.com>
2021-02-22 14:39:35 +0000

    network structure

--- LeNet_variation.py
+++ LeNet_variation.py
@@ -1,12 +1,12 @@
-import numpy as np
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Dropout, Activation, BatchNormalization, AveragePooling2D
-from tensorflow.keras.callbacks import ReduceLROnPlateau
-from keras.models import Sequential
 from keras.datasets import mnist
-from keras.utils import to_categorical 
+from keras.utils import to_categorical
+from keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Dropout, Activation, BatchNormalization, AveragePooling2D
+from keras.models import Model
+import numpy as np
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 from keras.models import load_model
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
 # Loading the MNIST datasets
 (x_train, labels_train), (x_test, labels_test) = mnist.load_data()
@@ -46,17 +46,17 @@ LeNet5_var.add(MaxPooling2D(pool_size=(2,2),strides=(2,2), padding='same'))
 LeNet5_var.add(Dropout(0.25))
 LeNet5_var.add(Flatten())
 # FC1
-LeNet5_var.add(tf.keras.layers.Dense(units=256,activation='relu'))
+LeNet5_var.add(Dense(units=256,activation='relu'))
 # B3
 LeNet5_var.add(BatchNormalization())
 LeNet5_var.add(Activation('relu'))
 # FC2
-LeNet5_var.add(tf.keras.layers.Dense(units=120,activation='relu'))
+LeNet5_var.add(Dense(units=120,activation='relu'))
 #B4
 LeNet5_var.add(BatchNormalization())
 LeNet5_var.add(Activation('relu'))
 # FC3
-LeNet5_var.add(tf.keras.layers.Dense(units=84,activation='relu'))
+LeNet5_var.add(Dense(units=84,activation='relu'))
 # B5
 LeNet5_var.add(BatchNormalization())
 LeNet5_var.add(Activation('relu'))
@@ -71,7 +71,7 @@ variable_learning_rate = ReduceLROnPlateau(monitor='loss', factor = 0.2, patienc
 LeNet5_var.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 history = LeNet5_var.fit(x_train,y_train, validation_data=(x_test,y_test), epochs=30, batch_size=64, callbacks = [variable_learning_rate])
 # SAVING THE MODEL
-LeNet5_var.save("LeNet_30_64.h5")
+LeNet5_var.save("LeNet_30_64_2.h5")
 
 plt.figure()
 plt.plot(history.history['loss'], label='training loss')
@@ -82,7 +82,7 @@ plt.legend()
 plt.show()
 
 # LOADING MODEL
-LeNet5_var = load_model("LeNet_30_64.h5")
+LeNet5_var = load_model("LeNet_30_64_2.h5")
 
 # TESTING
 outputs=LeNet5_var.predict(x_test)
